fbf934325b007f9b20b4796be6e0a52cd9aa8646
Ritik Sondhi <ritik.sondhi@propertise.com>
2026-02-06 10:50:51 +0530

    added test files and created ci pipeline

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('learning devops ci/cd pipeline')
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('starting tests...');
+setTimeout(() => console.log('waited for 3 secs'),3000);
+console.log('ending tests...');
